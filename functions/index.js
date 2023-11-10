52d0f86f14ec7d8785dd6b78b0c90e8a1a9bc107
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-10 12:28:31 -0500

    fixing login

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 import { onRequest } from "firebase-functions/v2/https";
 import express from 'express';
 import cors from 'cors';
-import { createUser } from "./src/users.js";
+import { createUser , login } from "./src/users.js";
 import { getAllRecipes ,createRecipe } from "./src/recipes.js";
 import { isAuthenticated } from "./src/middleware.js";
 
